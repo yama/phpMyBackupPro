4aaeea5d955c2e56a76158045fce517e6df6e251
yama <yamamoto@kyms.ne.jp>
2011-05-02 18:16:40 +0900

    翻訳更新

--- phpMyBackupPro/language/japanese.inc.php
+++ phpMyBackupPro/language/japanese.inc.php
@@ -26,7 +26,7 @@
  +--------------------------------------------------------------------------+
  | phpMyBackupPro Japanese translation                                      |
  +--------------------------------------------------------------------------+
- | Japanese translation 1.7                                                 |
+ | Japanese translation 2.1                                                 |
  +--------------------------------------------------------------------------+
 */
 
@@ -39,14 +39,14 @@ define('BD_CHARSET_EMAIL',"ISO-2022-JP"); // the charset used in your langauge f
 /*functions.inc.php*/
 define('F_START',"開始する");
 define('F_CONFIG',"設定");
-define('F_IMPORT',"インポート");
+define('F_IMPORT',"リストア");
 define('F_BACKUP',"バックアップ");
 define('F_SCHEDULE',"スケジュールバックアップ");
 define('F_DB_INFO',"データベース情報");
 define('F_SQL_QUERY',"SQL 問合せ");
 define('F_HELP',"ヘルプ");
 define('F_LOGOUT',"ログアウト");
-define('F_FOOTER',"新規リリースとニュースを取得しに %sphpMyBackupPro プロジェクトのサイト%s へ訪問します。");
+define('F_FOOTER',"新規リリースとニュースを取得するために %sphpMyBackupPro プロジェクトのサイト%s へ訪問します。");
 define('F_NOW_AVAILABLE',"phpMyBackupPro の新しいバージョンが %s".PMBP_WEBSITE."%s で利用できます。");
 define('F_SELECT_DB',"バックアップするデータベースを選択する");
 define('F_SELECT_ALL',"すべて選択する");
@@ -58,6 +58,7 @@ define('F_EX_COMP',"圧縮する");
 define('F_EX_OFF',"なし");
 define('F_EX_GZIP',"gzip");
 define('F_EX_ZIP',"zip");
+define('F_DEL_FAILED',"Failed to delete backup %s");
 define('F_FTP_1',"サーバーへの FTP 接続に失敗しました。");
 define('F_FTP_2',"ユーザーでログインに失敗しました。");
 define('F_FTP_3',"FTP アップロードに失敗しました");
@@ -65,29 +66,27 @@ define('F_FTP_4',"次のファイルとしてアップロードに成功しま�
 define('F_FTP_5',"ファイル「%s」の FTP 削除に失敗しました。");
 define('F_FTP_6',"FTP サーバー上でファイル「%s」の削除に成功しました。");
 define('F_FTP_7',"FTP サーバー上でファイル「%s」は利用できません。");
-define('F_MAIL_1',"ひとつの受信者電子メールが間違っています。");
-define('F_MAIL_2',"このメールは ".PMBP_WEBSITE." で実行中の phpMyBackupPro ".PMBP_VERSION." から送信されました。");
+define('F_MAIL_1',"送信先メールアドレスが間違っています。");
+define('F_MAIL_2',"このメールは phpMyBackupPro ".PMBP_VERSION." から送信しています。");
 define('F_MAIL_3',"読み込みができません。");
-define('F_MAIL_4',"次から MySQL バックアップする:");
+define('F_MAIL_4',"MySQLバックアップ:");
 define('F_MAIL_5',"メールを送信できません。");
-define('F_MAIL_6',"次に電子メールでファイルを送信することに成功しました:");
+define('F_MAIL_6',"ファイルをメール送信しました:");
 define('F_YES',"はい");
 define('F_NO',"いいえ");
 define('F_DURATION',"期間");
 define('F_SECONDS',"秒");
-define('F_WRONG_FILE',"これは、正確な .zip ファイルでないように見えます。それは phpMyBackupPro のバグかもしれません。");
+define('F_WRONG_FILE',"これは正確な .zip ファイルでないように見えます。それは phpMyBackupPro のバグかもしれません。");
 
 /*index.php*/
 define('I_SQL_ERROR',"エラー: 「configuration」に正確な MySQL データを挿入してください!");
 define('I_NAME',"これは phpMyBackupPro です。");
-define('I_WELCOME',"phpMyBackupPro は GNU GPL の元でライセンスされたフリーソフトウェアです。<br>
-援助が必要ならオンラインヘルプを試すか、%s に訪れてください。<br><br>
-次に何を行いたいかトップメニューから選択します!
-これが初めての phpMyBackupPro の使用なら設定から始めるべきです!
-ディレクトリ「export」とファイル「global_conf.php」の権限は 0777 に設定されていなければなりません。");
+define('I_WELCOME','phpMyBackupPro は GNU GPLライセンスの元で配布されているフリーソフトウェアです。<br />
+これが初めての phpMyBackupPro の使用なら<a href="config.php">設定</a>から始めてください。<br />
+サポートが必要ならオンラインヘルプを試すか、%s に訪れてください。<br />
+ディレクトリ「export」とファイル「global_conf.php」の権限は 0777 に設定されていなければなりません。');
 define('I_CONF_ERROR',"ファイル「".PMBP_GLOBAL_CONF."」が書き込めません!");
 define('I_DIR_ERROR',"ディレクトリ「".PMBP_EXPORT_DIR."」が書き込めません!");
-define('I_COMMENT_ERROR',"ファイル「".PMBP_EXPORT_DIR."comments.dat」が書き込めません!");
 define('PMBP_I_INFO',"システム情報");
 define('PMBP_I_SERVER',"サーバー");
 define('PMBP_I_TIME',"時間");
@@ -95,7 +94,7 @@ define('PMBP_I_PHP_VERS',"PHP バージョン");
 define('PMBP_I_MEM_LIMIT',"PHP メモリ制限");
 define('PMBP_I_SAFE_MODE',"セーフモード有効");
 define('PMBP_I_FTP',"FTP 転送可能");
-define('PMBP_I_MAIL',"電子メール送信可能");
+define('PMBP_I_MAIL',"メール添付で送信可能");
 define('PMBP_I_GZIP',"gzip 圧縮可能");
 define('PMBP_I_SQL_SERVER',"MySQL サーバー");
 define('PMBP_I_SQL_CLIENT',"MySQL クライアント");
@@ -121,20 +120,20 @@ define('C_FTP_PATH',"FTP パス");
 define('C_FTP_PASV',"FTP Passive を使いますか?");
 define('C_FTP_PORT',"FTP ポート");
 define('C_FTP_DEL',"FTP サーバーのファイルを削除する");
-define('C_EMAIL_USE',"電子メールを使用しますか?");
-define('C_EMAIL',"電子メールアドレス");
+define('C_EMAIL_USE',"メールを使用しますか?");
+define('C_EMAIL',"メールアドレス");
 define('C_STYLESHEET',"スキン");
 define('C_DATE',"日付のスタイル");
 define('C_DEL_TIME',"x 日経過したローカルバックアップを削除する");
 define('C_DEL_NUMBER',"データベース毎に最大 x ファイル保存します");
 define('C_TIMELIMIT',"PHP 時間制限");
-define('C_IMPORT_ERROR',"インポートエラーを表示しますか?");
+define('C_IMPORT_ERROR',"リストア時のエラーを表示しますか?");
 define('C_NO_LOGIN',"ログイン機能を無効にしますか?");
 define('C_LOGIN',"HTTP 認証しますか?");
 define('C_DIR_BACKUP',"ディレクトリバックアップを有効にしますか?");
 define('C_DIR_REC',"サブディレクトリにディレクトリバックアップしますか?");
 define('C_CONFIRM',"確認レベル");
-define('C_CONFIRM_1',"空、削除、インポート");
+define('C_CONFIRM_1',"空、削除、リストア");
 define('C_CONFIRM_2',"... all");
 define('C_CONFIRM_3',"... ALL");
 define('C_CONFIRM_4',"何も確認しません");
@@ -145,7 +144,7 @@ define('PMBP_C_SYSTEM_VAL',"システム変数");
 define('PMBP_C_SYS_WARNING',"これらのシステム変数は phpMyBackupPro にて管理されています。もし何をしているか知らないならそれらを編集しないでください!");
 define('C_TITLE_SQL',"SQL データ");
 define('C_TITLE_FTP',"FTP でバックアップ");
-define('C_TITLE_EMAIL',"電子メールでバックアップ");
+define('C_TITLE_EMAIL',"メール添付でバックアップ");
 define('C_TITLE_STYLE',"phpMyBackupPro のスタイル");
 define('C_TITLE_DELETE',"バックアップファイルの自動削除");
 define('C_TITLE_CONFIG',"さらなる設定項目");
@@ -161,7 +160,7 @@ define('C_SAVE',"データ保存");
 
 /*import.php*/
 define('IM_ERROR',"%d 個のエラーが発生しました。You can use 'empty database' to be sure the database does not contain any tables.");
-define('IM_SUCCESS',"インポートに成功しました");
+define('IM_SUCCESS',"リストアに成功しました");
 define('IM_TABLES',"tables and");
 define('IM_ROWS',"rows");
 
@@ -171,32 +170,34 @@ define('B_DELETED',"ファイの削除に成功しました。");
 define('B_DELETED_ALL',"すべてのファイルを削除することに成功しました。");
 define('B_NO_FILES',"現在バックアップファイルがありません。");
 define('B_DELETE_ALL_2',"すべてのバックアップを削除する");
-define('B_IMPORT_ALL',"すべてのバックアップをインポートする");
+define('B_IMPORT_ALL',"最新のバックアップからリストアする");
 define('B_EMPTY_ALL',"すべてのデータベースを空にする");
 define('B_EMPTY_DB',"データベースを空にする");
 define('B_DELETE_ALL',"すべてのバックアップを削除する");
 define('B_INFO',"情報");
 define('B_VIEW',"閲覧");
 define('B_DOWNLOAD',"ダウンロード");
-define('B_IMPORT',"インポート");
+define('B_IMPORT',"リストア");
+define('B_IMPORT_FRAG',"フラグメント単位で実行");
 define('B_DELETE',"削除");
 define('B_CONF_EMPTY_DB',"本当にデータベースを空にしますか?");
 define('B_CONF_DEL_ALL',"本当にこのデータベースのすべてのバックアップを削除しますか?");
-define('B_CONF_IMP',"本当にこのバックアップをインポートしますか?");
+define('B_CONF_IMP',"本当にこのバックアップをリストアしますか?");
 define('B_CONF_DEL',"本当にこのバックアップを削除しますか?");
 define('B_CONF_EMPT_ALL',"本当にすべてのデータベースを空にしますか?");
-define('B_CONF_IMP_ALL',"本当に最後のバックアップからすべてインポートしますか?");
+define('B_CONF_IMP_ALL',"本当に最新のバックアップからすべてリストアしますか?");
 define('B_CONF_DEL_ALL_2',"本当にすべてのバックアップを削除しますか?");
 define('B_LAST_BACKUP',"Last backup built on");
 define('B_SIZE_SUM',"すべてのバックアップの総容量");
 
 /*backup.php*/
-define('EX_SAVED',"次のようにファイルの保存に成功しました:");
+define('EX_SAVED',"ファイルの保存に成功しました。「リストア」で確認できます。:");
 define('EX_NO_DB',"データベースが選択されていません");
 define('EX_EXPORT',"バックアップ");
 define('EX_NOT_SAVED',"データベース %s を保管できません。");
 define('EX_DIRS',"バックアップ先 FTP サーバーを選択する");
-define('EX_DIRS_MAN',"Enter more directory paths relative to phpMyBackupPro directory.<br>Separate with '|'");
+define('EX_DIRS_MAN',"Enter more directory paths relative to phpMyBackupPro directory.<br />Separate with '|'");
+define('EX_PACKED',"Pack all in one ZIP file");
 define('PMBP_EX_NO_AVAILABLE',"データベース %s は利用できません。");
 define('PMBP_EXS_UPDATE_DIRS',"ディレクトリ一覧を更新する");
 define('PMBP_EX_NO_ARGV',"使用例:\n$ php backup.php db1,db2,db3
@@ -215,7 +216,7 @@ define('EXS_WEEK',"week");
 define('EXS_WEEKS',"weeks");
 define('EXS_MONTH',"month");
 define('EXS_SHOW',"スクリプトを表示する");
-define('PMBP_EXS_INCL',"�Хå����åפ�Ԥ�����php�ե�����ˤ��Υ�����ץȤ��������Ʋ�����(%s)");
+define('PMBP_EXS_INCL',"バックアップを行いたいphpファイルにこのスクリプトを挿入して下さい(%s)");
 define('PMBP_EXS_SAVE',"or save this script to a new file (will overwrite an existing file!)");
 
 /*file_info.php*/
@@ -223,7 +224,7 @@ define('INF_INFO',"情報");
 define('INF_DATE',"日付");
 define('INF_DB',"データベース");
 define('INF_SIZE',"バックアックサイズ");
-define('INF_COMP',"圧縮するかどうか");
+define('INF_COMP',"圧縮形式");
 define('INF_DROP',"「DROP TABLE」を含む");
 define('INF_TABLES',"テーブルを含む");
 define('INF_DATA',"データを含む");
@@ -235,7 +236,7 @@ define('DB_NAME',"データベースの名前");
 define('DB_NUM_TABLES',"テーブル数");
 define('DB_NUM_ROWS',"行数");
 define('DB_SIZE',"サイズ");
-define('DB_DIFF',"���̤��Хå����åץե���������̤Ȱ�äƤ⹽���ޤ���!");
+define('DB_DIFF',"※ここで示されるサイズはバックアップファイルのサイズとは異なります。");
 define('DB_NO_DB',"データベースは利用できません。");
 define('DB_TABLES',"テーブル情報");
 define('DB_TAB_TITLE',"tables of database ");
@@ -247,10 +248,10 @@ define('SQ_ERROR',"照会中にエラーが発生しました。行番号: ");
 define('SQ_SUCCESS',"実行に成功しました");
 define('SQ_RESULT',"照会結果");
 define('SQ_AFFECTED',"影響行数");
-define('SQ_WARNING',"注意: このページはデータベースへ単純な SQL 照会を送信する為に構築されています。不注意な実行はデータベースを破壊する場合があります!");
+define('SQ_WARNING',"注意: このページでは簡単なSQLクエリ文を実行できます。誤ったクエリ文はデータベースを破壊することがあるので、不注意な実行は避けてください。");
 define('SQ_SELECT_DB',"データベースを選択");
-define('SQ_INSERT',"ここに SQL 照会を挿入します");
-define('SQ_FILE',"SQL ファイルをアップロードするファイル");
+define('SQ_INSERT',"SQLクエリ文を挿入します");
+define('SQ_FILE',"SQLクエリ文が書かれたファイルをアップロードする場合はこちら");
 define('SQ_SEND',"実行する");
 
 /*login.php*/
@@ -259,5 +260,26 @@ define('LI_USER',"ユーザー名");
 define('LI_PASSWD',"パスワード");
 define('LI_LOGIN',"ログイン");
 define('LI_LOGED_OUT',"安全にログアウトアウトしました!");
-define('LI_NOT_LOGED_OUT',"安全ではないログアウトです!<br>安全なログアウトは間違ったパスワードの入力です。");
+define('LI_NOT_LOGED_OUT',"安全ではないログアウトです!<br />安全なログアウトは間違ったパスワードの入力です。");
+
+/*big_import.php*/
+define('BI_IMPORTING_FILE',"Importing file");
+define('BI_INTO_DB',"Into database");
+define('BI_SESSION_NO',"Session number");
+define('BI_STARTING_LINE',"Starting at line");
+define('BI_STOPPING_LINE',"Stopping at line");
+define('BI_QUERY_NO',"Number of queries performed");
+define('BI_BYTE_NO',"Number of bytes processed yet");
+define('BI_DURATION',"Duration of last session");
+define('BI_THIS_LAST',"this session/total");
+define('BI_END',"End of file reached, import seems to be OK");
+define('BI_RESTART',"Restart import of file ");
+define('BI_SCRIPT_RUNNING',"This script is still running!<br />Please wait until the end of the file is reached");
+define('BI_CONTINUE',"Continue from the line");
+define('BI_ENABLE_JS',"Enable JavaScript to continue automatically");
+define('BI_BROKEN_ZIP',"The ZIP file seems to be broken");
+define('BI_WRONG_FILE',"Stopped at line %s.<br />The current query includes more than %s dump lines. That happens if your backup file was created
+by some tool which didn't place a semicolon followed by a linebreak at the end of each query, or if your backup file contains extended inserts.");
+
+define('I_COMMENT_ERROR',"ファイル「".PMBP_EXPORT_DIR."comments.dat」が書き込めません!");
 ?>
